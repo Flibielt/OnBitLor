fa0a30ec6e7e9886454c9d05e26a609b217e8e2f
Csaba Hermann <hcsabi98@gmail.com>
2020-02-18 14:43:09 +0100

    Created project structure

---
+++ etrainer/src/main/java/hu/deach/etrainer/EtrainerApplication.java
@@ -0,0 +1,13 @@
+package hu.deach.etrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtrainerApplication.class, args);
+	}
+
+}
---
+++ etrainer/src/test/java/hu/deach/etrainer/EtrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.deach.etrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
