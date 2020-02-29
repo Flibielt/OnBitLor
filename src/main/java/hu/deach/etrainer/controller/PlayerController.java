e1c3becc78626f8f28d55a16247e4fd0b10049d1
Csaba Hermann <hcsabi98@gmail.com>
2020-02-29 22:33:09 +0100

    Added endpoints to PlayerController
    Check username and email availability

--- src/main/java/hu/deach/etrainer/controller/PlayerController.java
+++ src/main/java/hu/deach/etrainer/controller/PlayerController.java
@@ -2,30 +2,21 @@ package hu.deach.etrainer.controller;
 
 import hu.deach.etrainer.dto.PlayerDto;
 import hu.deach.etrainer.exception.ResourceNotFoundException;
+import hu.deach.etrainer.model.PlayerIdentityAvailability;
 import hu.deach.etrainer.service.PlayerService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @CrossOrigin
 @RestController
-@RequestMapping("/players")
+@RequestMapping("/api/player")
 public class PlayerController {
 
     @Autowired
     private PlayerService playerService;
-
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    @PostMapping("/sign-up")
-    public void signUp(@RequestBody PlayerDto playerDto) {
-        playerDto.setPassword(bCryptPasswordEncoder.encode(playerDto.getPassword()));
-        playerService.save(playerDto);
-    }
 
     @GetMapping("/{username}")
     @PreAuthorize("hasRole('ROLE_USER')")
@@ -35,6 +26,18 @@ public class PlayerController {
             throw new ResourceNotFoundException("Player", "username", username);
         }
         return playerDto;
+    }
+
+    @GetMapping("/checkUsernameAvailability")
+    public PlayerIdentityAvailability checkUsernameAvailability(@RequestParam(value = "username") String username) {
+        Boolean isAvailable = !playerService.existsByEmail(username);
+        return new PlayerIdentityAvailability(isAvailable);
+    }
+
+    @GetMapping("/checkEmailAvailability")
+    public PlayerIdentityAvailability checkEmailAvailability(@RequestParam(value = "email") String email) {
+        Boolean isAvailable = !playerService.existsByEmail(email);
+        return new PlayerIdentityAvailability(isAvailable);
     }
 
 }
---
+++ src/main/java/hu/deach/etrainer/model/PlayerIdentityAvailability.java
@@ -0,0 +1,10 @@
+package hu.deach.etrainer.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PlayerIdentityAvailability {
+    Boolean isAvailable;
+}
