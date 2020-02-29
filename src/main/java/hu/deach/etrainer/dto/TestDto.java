5caae27f545ba7f877f29243c07b60233d6fb043
Csaba Hermann <hcsabi98@gmail.com>
2020-02-29 15:14:36 +0100

    Created DTOs for the test entities
    Created TestDto and TestResultDto

---
+++ src/main/java/hu/deach/etrainer/dto/TestDto.java
@@ -0,0 +1,12 @@
+package hu.deach.etrainer.dto;
+
+import lombok.Data;
+
+@Data
+public class TestDto {
+
+    private Long id;
+
+    private String name;
+
+}
---
+++ src/main/java/hu/deach/etrainer/dto/TestResultDto.java
@@ -0,0 +1,14 @@
+package hu.deach.etrainer.dto;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class TestResultDto {
+
+    private Long playerId;
+    private Long testId;
+    private Date date;
+    private Float result;
+}
