f90031760b5ae7a9532686c6c7c87bed9d03aab1
Csaba Hermann <hcsabi98@gmail.com>
2020-03-03 23:05:07 +0100

    Created some API call functions

--- frontend/src/util/APIUtils.js
+++ frontend/src/util/APIUtils.js
@@ -30,4 +30,52 @@ export function getAllGames() {
     });
 }
 
+export function login(loginRequest) {
+    return request({
+        url: API_BASE_URL + "/auth/signin",
+        method: 'POST',
+        body: JSON.stringify(loginRequest)
+    });
+}
+
+export function signUp(signUpRequest) {
+    return request({
+        url: API_BASE_URL + "/auth/signup",
+        method: 'POST',
+        body: JSON.stringify(signUpRequest)
+    });
+}
+
+export function checkUsernameAvailability(username) {
+    return request({
+        url: API_BASE_URL + "/player/checkUsernameAvailability?username=" + username,
+        method: 'GET'
+    });
+}
+
+export function checkEmailAvailability(email) {
+    return request({
+        url: API_BASE_URL + "/player/checkEmailAvailability?email=" + email,
+        method: 'GET'
+    });
+}
+
+export function getCurrentUser() {
+    if (!localStorage.getItem(ACCESS_TOKEN)) {
+        return Promise.reject("No access token set.");
+    }
+
+    return request({
+        url: API_BASE_URL + "/player/me",
+        method: 'GET'
+    });
+}
+
+export function getUserProfile(username) {
+    return request({
+        url: API_BASE_URL + "/player/" + username,
+        method: 'GET'
+    });
+}
+
 //todo: implement more API calls
