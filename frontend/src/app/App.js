c7e642861d24c141cb98d467b615df9f353eafe1
Csaba Hermann <hcsabi98@gmail.com>
2020-03-09 20:39:14 +0100

    User can view profiles, if signed in

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -108,9 +108,9 @@ class App extends Component {
                 <Route path="/login" 
                   render={(props) => <Login onLogin={this.handleLogin} {...props} />}></Route>
                 <Route path="/signup" component={Signup}></Route>
-                <Route path="/users/:username" 
+                <PrivateRoute path="/users/:username"
                   render={(props) => <Profile isAuthenticated={this.state.isAuthenticated} currentUser={this.state.currentUser} {...props}  />}>
-                </Route>
+                </PrivateRoute>
                 <PrivateRoute authenticated={this.state.isAuthenticated} path="/poll/new" component={NewPoll} handleLogout={this.handleLogout}></PrivateRoute>
                 <Route component={NotFound}></Route>
               </Switch>
