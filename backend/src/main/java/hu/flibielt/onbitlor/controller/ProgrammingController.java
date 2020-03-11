cffc22a91e794bc09e8d57e0a836a4f1029bb69e
Csaba Hermann <hcsabi98@gmail.com>
2020-03-11 22:24:44 +0100

    Added endpoints to ProgrammingController
    Add new result, get result by id and get results by programming competition

--- backend/src/main/java/hu/flibielt/onbitlor/controller/ProgrammingController.java
+++ backend/src/main/java/hu/flibielt/onbitlor/controller/ProgrammingController.java
@@ -1,10 +1,15 @@
 package hu.flibielt.onbitlor.controller;
 
 import hu.flibielt.onbitlor.dto.ProgrammingDto;
+import hu.flibielt.onbitlor.dto.ProgrammingStatisticDto;
 import hu.flibielt.onbitlor.model.ApiResponse;
 import hu.flibielt.onbitlor.model.ProgrammingNameAvailability;
 import hu.flibielt.onbitlor.model.ProgrammingRequest;
+import hu.flibielt.onbitlor.model.ProgrammingResultRequest;
+import hu.flibielt.onbitlor.security.CurrentUser;
+import hu.flibielt.onbitlor.security.UserPrincipal;
 import hu.flibielt.onbitlor.service.ProgrammingService;
+import hu.flibielt.onbitlor.service.ProgrammingStatisticService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +19,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.Date;
 
 @Slf4j
 @RestController
@@ -22,6 +28,9 @@ public class ProgrammingController {
 
     @Autowired
     private ProgrammingService programmingService;
+
+    @Autowired
+    private ProgrammingStatisticService programmingStatisticService;
 
     @PostMapping
     public ResponseEntity<?> addProgramming(@RequestBody ProgrammingRequest request) {
@@ -50,5 +59,38 @@ public class ProgrammingController {
     public ProgrammingNameAvailability checkNameAvailability(@RequestParam(value = "name") String name) {
         Boolean isAvailable = programmingService.existsByName(name);
         return new ProgrammingNameAvailability(isAvailable);
+    }
+
+    @PostMapping("/addResult")
+    public ResponseEntity<?> addNewResult(@CurrentUser UserPrincipal userPrincipal, ProgrammingResultRequest request) {
+        if (!programmingService.existsByName(request.getProgrammingName())) {
+            return ResponseEntity.badRequest()
+                    .body(new ApiResponse(false, "Cannot find the given programming competition"));
+        }
+        Long programmingId = programmingService.getId(request.getProgrammingName());
+
+        ProgrammingStatisticDto programmingStatisticDto = new ProgrammingStatisticDto();
+        programmingStatisticDto.setPlayerId(userPrincipal.getId());
+        programmingStatisticDto.setProgrammingId(programmingId);
+        programmingStatisticDto.setDate(new Date());
+        programmingStatisticDto.setScore(request.getScore());
+
+        ProgrammingStatisticDto savedResult = programmingStatisticService.save(programmingStatisticDto);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/result/{resultId}")
+                .buildAndExpand(savedResult.getId()).toUri();
+
+        return ResponseEntity.created(location)
+                .body(new ApiResponse(true, "Created new programming result"));
+    }
+
+    @GetMapping("/result/{id}")
+    public ProgrammingStatisticDto getProgrammingResult(@PathVariable(value = "id") Long id) {
+        return programmingStatisticService.findById(id);
+    }
+
+    @GetMapping("/results/{programmingId}")
+    public ArrayList<ProgrammingStatisticDto> getAllResultInCompetition(@PathVariable(value = "programmingId") Long programmingId) {
+        return programmingStatisticService.findAll();
     }
 }
