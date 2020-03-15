be763ba88103db665149ac98457a5bf2390f534b
Csaba Hermann <hcsabi98@gmail.com>
2020-03-15 22:40:31 +0100

    Created new components for tests

---
+++ frontend/src/test/NewTest.js
@@ -0,0 +1,5 @@
+import React, {Component} from "react";
+
+class NewTest extends Component{
+
+}
---
+++ frontend/src/test/TestResultList.js
@@ -0,0 +1,5 @@
+import React, {Component} from "react";
+
+class TestResultList extends Component {
+
+}
