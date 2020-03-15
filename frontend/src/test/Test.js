2a8f307419122e2c4fa1c75fcf4ed46cb22ae0fe
Csaba Hermann <hcsabi98@gmail.com>
2020-03-15 22:58:30 +0100

    Created Test.js component

--- frontend/src/test/Test.js
+++ frontend/src/test/Test.js
@@ -1,5 +1,23 @@
-import React, {Component} from "react";
+import React, { Component } from 'react';
 
-class Test extends Component{
-
+class Test extends Component {
+    render() {
+        return (
+            <div className="content">
+                <div className="test-info">
+                    <h2 className="name">
+                        {this.props.test.name}
+                    </h2>
+                    <p className="description">
+                        {this.props.test.description}
+                    </p>
+                    <p className="description">
+                        Bit: {this.props.test.bit}
+                    </p>
+                </div>
+            </div>
+        );
+    }
 }
+
+export default Test;
