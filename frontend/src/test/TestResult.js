0876bf27ad5a0af45cde3c0dfa359b968565813a
Csaba Hermann <hcsabi98@gmail.com>
2020-03-15 23:21:19 +0100

    Created component TestResult.js

--- frontend/src/test/TestResult.js
+++ frontend/src/test/TestResult.js
@@ -1,5 +1,32 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-class TestResult extends Component {
+class TestResult extends Component{
+    render() {
+        return (
+            <div className="content">
+                <div className="position">
+                    <h2 className="title">
+                        {this.props.position}.
+                    </h2>
+                </div>
+                <div className="summary">
+                    <h2 className="title">
+                        Result: {this.props.result.result}
+                    </h2>
 
+                    <p className="description">
+                        {this.props.result.username}
+                    </p>
+                    <p className="description">
+                        {this.props.result.firstName} {this.props.result.lastName}
+                    </p>
+                    <p className="description">
+                        {this.props.result.date}
+                    </p>
+                </div>
+            </div>
+        );
+    }
 }
+
+export default TestResult;
