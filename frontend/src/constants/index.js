880c16e9a4fd1cea110272935b8b1c852d3f1506
Csaba Hermann <hcsabi98@gmail.com>
2020-03-17 22:14:30 +0100

    Updated constants

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,14 +1,9 @@
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
 export const ACCESS_TOKEN = 'accessToken';
 
-export const POLL_LIST_SIZE = 30;
-export const GAME_LIST_SIZE = 30;
 export const PROGRAMMING_LIST_SIZE = 30;
-export const MAX_CHOICES = 6;
-export const POLL_QUESTION_MAX_LENGTH = 140;
-export const POLL_CHOICE_MAX_LENGTH = 40;
 
-export const NAME_MIN_LENGTH = 4;
+export const NAME_MIN_LENGTH = 1;
 export const NAME_MAX_LENGTH = 40;
 
 export const USERNAME_MIN_LENGTH = 3;
