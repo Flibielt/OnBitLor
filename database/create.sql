f382db9ef6c9548c82c060ad6e9d268afa0c751c
Csaba Hermann <hcsabi98@gmail.com>
2020-03-17 21:37:12 +0100

    Created example database

---
+++ database/create.sql
@@ -0,0 +1,63 @@
+CREATE TABLE `players` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) UNIQUE NOT NULL,
+  `username` varchar(255) UNIQUE NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `bit` int
+);
+
+CREATE TABLE `roles` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE `user_roles` (
+  `player_id` int,
+  `role_id` int,
+  PRIMARY KEY (`player_id`, `role_id`)
+);
+
+CREATE TABLE `programming` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255) UNIQUE NOT NULL,
+  `description` text NOT NULL,
+  `bit` int
+);
+
+CREATE TABLE `programming_statistics` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `programming_id` int NOT NULL,
+  `player_id` int NOT NULL,
+  `date` datetime NOT NULL,
+  `score` int NOT NULL
+);
+
+CREATE TABLE `tests` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255) UNIQUE NOT NULL,
+  `description` text NOT NULL,
+  `bit` int
+);
+
+CREATE TABLE `test_results` (
+  `player_id` int,
+  `test_id` int,
+  `date` datetime NOT NULL,
+  `result` long,
+  PRIMARY KEY (`player_id`, `test_id`)
+);
+
+ALTER TABLE `programming_statistics` ADD FOREIGN KEY (`programming_id`) REFERENCES `programming` (`id`);
+
+ALTER TABLE `programming_statistics` ADD FOREIGN KEY (`player_id`) REFERENCES `players` (`id`);
+
+ALTER TABLE `user_roles` ADD FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
+
+ALTER TABLE `user_roles` ADD FOREIGN KEY (`player_id`) REFERENCES `players` (`id`);
+
+ALTER TABLE `test_results` ADD FOREIGN KEY (`test_id`) REFERENCES `tests` (`id`);
+
+ALTER TABLE `test_results` ADD FOREIGN KEY (`player_id`) REFERENCES `players` (`id`);
+
---
+++ database/insert.sql
@@ -0,0 +1,9 @@
+insert into roles values(1, 'USER_ROLE');
+insert into roles values(2, 'USER_LEADER');
+insert into roles values(3, 'USER_ADMIN');
+
+insert into programming values(1, 'Red Flower Hell I.', 'In the elements of Red Flower Hell series we are going to develop intelligent agents to collect poppies. After this we would also like develop AGI agents in this environment. It is therefore essential to know that how many poppies can a human player collect?', 10);
+insert into programming values(2, 'Red Flower Hell II.', 'In the elements of Red Flower Hell series we are going to develop intelligent agents to collect poppies. After this we would also like develop AGI agents in this environment. It is therefore essential to know that how many poppies can a human player collect?', 20);
+
+insert into tests values(1, 'SMNIST', 'The Semantic "MNIST". Have you seen the movie, Rain Man? Do you remember the scene when Dustin Hoffman can count the exact number of toothpicks on the floor in the blink of an eye. We don\'t count toothpicks but dots in an image.', 20);
+insert into tests values(2, 'BrainB', 'This project aims to create an interdisciplinary research program that links human perception, infomation theory, and talent search in the field of e-sports.', 20);
