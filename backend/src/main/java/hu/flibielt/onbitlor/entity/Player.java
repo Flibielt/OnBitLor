9b8f8417b45bfc34f3d64b941b30303f7f4a0bce
Csaba Hermann <hcsabi98@gmail.com>
2020-03-19 18:46:03 +0100

    Bug fix
    Changed ID generation strategy

--- backend/src/main/java/hu/flibielt/onbitlor/entity/Player.java
+++ backend/src/main/java/hu/flibielt/onbitlor/entity/Player.java
@@ -11,7 +11,7 @@ import java.util.Set;
 public class Player {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "first_name")
